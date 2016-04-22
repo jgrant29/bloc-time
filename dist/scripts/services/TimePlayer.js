68c2e79b3b8a071a20ed0f9f97d5e1a81e51bc93
Justin Grant <jmgrant702@gmail.com>
2016-04-21 18:54:56 -0600

    checkpoint-4-complete

--- dist/scripts/services/TimePlayer.js
+++ dist/scripts/services/TimePlayer.js
@@ -4,16 +4,18 @@
 
     TimePlayer.format = 'mm:ss';
     TimePlayer.relax = false;
-    var WORKTIME = 5;
-    var BREAKTIME = 4;
-    TimePlayer.pomodoro = 3;
-
+    var WORKTIME = 1500;
+    var BREAKTIME = 300;
+    var BIGBREAK = 1800;
+    TimePlayer.pomodoro = 1500;
+    var howManyTimes = 0
     var stop;
     TimePlayer.work = function() {
       stop = $interval(function() {
         if (TimePlayer.pomodoro > 0 ) {
           TimePlayer.pomodoro = TimePlayer.pomodoro - 1;
           TimePlayer.buttonClicked = true;
+          
         } else {
           if(TimePlayer.relax){
             // prepare timer for work
@@ -22,11 +24,19 @@
             TimePlayer.relax = false;
             TimePlayer.buttonClicked = false;
           }else{
+            if (howManyTimes > 0 && howManyTimes % 4 == 0) {
+              $interval.cancel(stop);
+              TimePlayer.pomodoro = BIGBREAK;
+              TimePlayer.relax = true;
+              TimePlayer.buttonClicked = false;
+            }else{
             // prepare timer for break
-            $interval.cancel(stop);
-            TimePlayer.pomodoro = BREAKTIME;
-            TimePlayer.relax = true;
-            TimePlayer.buttonClicked = false;
+              $interval.cancel(stop);
+              TimePlayer.pomodoro = BREAKTIME;
+              TimePlayer.relax = true;
+              TimePlayer.buttonClicked = false;
+            }
+            howManyTimes++
           }
         }
       }, 1000);
@@ -44,7 +54,7 @@
       $interval.cancel(stop);
       TimePlayer.pomodoro = 1500;
       TimePlayer.buttonClicked = false;
-
+      var howManyTimes = 0
     };
 
     return TimePlayer;
